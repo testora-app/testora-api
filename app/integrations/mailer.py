b92b531ee5b7e905dfee5a2ec937a9623486bf2d
Jnr <averagewifiuser@gmail.com>
2025-07-03 15:55:09 +0000

    fix: removed unused logging from mailer

--- app/integrations/mailer.py
+++ app/integrations/mailer.py
@@ -2,7 +2,6 @@ import requests
 from globals import SMTP2GO_API_KEY
 
 from flask import render_template
-from logging import error as log_error
 
 
 class Mailer(object):
@@ -43,9 +42,7 @@ class Mailer(object):
         }
 
         response = requests.post(self.api_url, headers=headers, json=payload)
-        log_error(response.json())
         response.raise_for_status()  # Raise an exception for HTTP errors
-
         return response.json()
 
 
