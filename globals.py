dc3c5132e49fc0de5eb4f75a45769ba4b2582903
Jnr <averagewifiuser@gmail.com>
2025-07-09 23:42:27 +0000

    chore: updated frontend base url

--- globals.py
+++ globals.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SMTP2GO_API_KEY = os.getenv("SMTP2GO_API_KEY")
-FRONTEND_BASE_URL = os.getenv("FRONTEND_BASE_URL", "http://localhost:3000")
+FRONTEND_BASE_URL = os.getenv("FRONTEND_BASE_URL", "http://localhost:3050")
 ONE_SIGNAL_APP_ID = os.getenv("ONE_SIGNAL_APP_ID")
 ONE_SIGNAL_REST_API_KEY = os.getenv("ONE_SIGNAL_REST_API_KEY")
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
